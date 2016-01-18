3fc750eb967a8ed467b17a37bac4b0170daee156
Sewic <bassewic@outlook.com>
2016-01-18 15:46:04 +0100

    Change1

--- Assignment_1/src/se/mah/ke/k3lara/Main.java
+++ Assignment_1/src/se/mah/ke/k3lara/Main.java
@@ -6,10 +6,32 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JScrollBar;
+import javax.swing.JTextPane;
+import javax.swing.JEditorPane;
+import javax.swing.BoxLayout;
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
+import javax.swing.JButton;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -33,11 +55,52 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 688, 467);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnArchive = new JMenu("Archive");
+		menuBar.add(mnArchive);
+		
+		JMenuItem mntmPrint = new JMenuItem("Print...");
+		mnArchive.add(mntmPrint);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnArchive.add(mntmExit);
+		
+		JMenu mnMember = new JMenu("Member");
+		menuBar.add(mnMember);
+		
+		JMenuItem mntmNewMember = new JMenuItem("New Member");
+		mnMember.add(mntmNewMember);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Find Member");
+		mnMember.add(mntmNewMenuItem);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
+		
+		JMenuItem mntmHelp = new JMenuItem("Help");
+		mnHelp.add(mntmHelp);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 108, 385);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setBounds(573, 373, 89, 23);
+		contentPane.add(btnSave);
 	}
-
 }
